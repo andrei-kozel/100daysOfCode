fca788c9f56722fb7db55815f1474945dcb5a493
Andrei Kozel <bexxxa38@gmail.com>
2020-01-20 21:54:24 +0100

    generator test

--- JavaScript/day011/generators.js
+++ JavaScript/day011/generators.js
@@ -13,3 +13,18 @@ console.log("2: ");
 console.log(weaponsIterator.next());
 console.log("3: ");
 console.log(weaponsIterator.next());
+
+function* NinjaGenerator(action) {
+  const imposter = yield "Hattori " + action;
+  console.log(imposter);
+
+  yield "Yoshi (" + imposter + ") " + action;
+}
+
+const ninjaIterator = NinjaGenerator("skulk");
+
+const result1 = ninjaIterator.next();
+const result2 = ninjaIterator.next("Hanzo");
+
+console.log(result1);
+console.log(result2);
