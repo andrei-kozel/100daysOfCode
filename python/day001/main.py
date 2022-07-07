f30decea8fea9bc88394bbd2463b0b72ebe27a98
Andrei Kozel <kozel.andrei.94@gmail.com>
2022-07-07 18:49:58 +0200

    Day 1 project :white_check_mark:

---
+++ python/day001/main.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator! ")
+city = input("What is the name of your city?\n")
+pet_name=input("What's your pet's name?\n")
+print("Your band name is " + city + " " + pet_name)
