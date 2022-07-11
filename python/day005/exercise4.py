3641efc2920098ce716b26f81b5e289bffba7231
Andrei Kozel <kozel.andrei.94@gmail.com>
2022-07-11 09:53:36 +0200

    exercise 4 :white_check_mark:

---
+++ python/day005/exercise4.py
@@ -0,0 +1,11 @@
+# You are going to write a program that automatically prints the solution to the FizzBuzz game.
+# Write your code below this row 👇
+for num in range(1, 101):
+    if num % 15 == 0:
+        print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
