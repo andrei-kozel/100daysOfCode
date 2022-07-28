61ba4a9bdf07f1c830fffeb330d459178dced960
Andrei Kozel <kozel.andrei.94@gmail.com>
2022-07-28 13:08:47 +0200

    exercise 2 :white_check_mark:

---
+++ python/day013/exercise2.py
@@ -0,0 +1,17 @@
+# Read this the code
+# Spot the problems 🐞.
+# Modify the code to fix the program.
+
+# before year = input("Which year do you want to check?")
+year = int(input("Which year do you want to check?"))
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year.")
+        else:
+            print("Not leap year.")
+    else:
+        print("Leap year.")
+else:
+    print("Not leap year.")
