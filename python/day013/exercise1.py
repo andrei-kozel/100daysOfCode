dd1ebed7e2dd6c543a2a8c22ddf8d8eb0f61557a
Andrei Kozel <kozel.andrei.94@gmail.com>
2022-07-28 13:07:07 +0200

    exercise 1 :white_check_mark:

---
+++ python/day013/exercise1.py
@@ -0,0 +1,10 @@
+# Read this the code
+# Spot the problems 🐞.
+# Modify the code to fix the program.
+
+number = int(input("Which number do you want to check?"))
+
+if number % 2 == 0:  # before: if number % 2 = 0:
+    print("This is an even number.")
+else:
+    print("This is an odd number.")
